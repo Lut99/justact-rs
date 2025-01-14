3fca4b60e83642f23bda336c774c8a9d1d05090d
Lut99 <mullertim99@gmail.com>
2025-01-14 16:12:23 +0100

    Updated `Times` to something working

--- src/times.rs
+++ src/times.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    11 Dec 2024, 14:57:21
 //  Last edited:
-//    13 Jan 2025, 16:24:08
+//    14 Jan 2025, 16:11:58
 //  Auto updated?
 //    Yes
 //
@@ -13,35 +13,105 @@
 //!   is.
 //
 
+use std::convert::Infallible;
 use std::hash::Hash;
 
 use auto_traits::pointer_impls;
 
-use crate::auxillary::Timed;
-use crate::collections::set::Set;
+use crate::collections::set::{Set, SetSync};
+
+
+/***** AUXILLARY *****/
+/// Convenience wrapper around [`Times`] for when they are [infallible](std::convert::Infallible).
+pub trait InfallibleTimes: Times<Error = Infallible> {
+    /// Returns all the current times.
+    ///
+    /// This is used by auditors or other agents at the moment they first receive an enactment to
+    /// see if it was done using a current agreement. I.e., if the action/agreement was at a time
+    /// which is in the returned set, it is considered current.
+    ///
+    /// # Returns
+    /// Some set of [`Self::Timestamp`](Times::Timestamp)s that indicate which are current.
+    fn current(&self) -> Self::Subset;
+}
+impl<T: Times<Error = Infallible>> InfallibleTimes for T {
+    #[inline]
+    fn current(&self) -> Self::Subset {
+        // SAFETY: It is physically impossible for users to express `Err(...)` due to the inability
+        // to construct `Infallible`
+        unsafe { <T as Times>::current(self).unwrap_unchecked() }
+    }
+}
+
+/// Convenience wrapper around [`TimesSync`]s for when they are
+/// [infallible](std::convert::Infallible).
+pub trait InfallibleTimesSync: TimesSync<Error = Infallible> {
+    /// Adds a given timestamp to the current ones.
+    ///
+    /// This function should always imply also calling [`SetSync::add()`] for the given element.
+    /// Remember, after all, that the set of current times needs to be a _subset_ of all times.
+    ///
+    /// # Arguments
+    /// - `timestamp`: A new [`Times::Timestamp`] to mark as current.
+    ///
+    /// # Returns
+    /// True if the element was already marked as current, or false otherwise.
+    fn add_current(&mut self, timestamp: Self::Timestamp) -> bool;
+}
+impl<T: TimesSync<Error = Infallible>> InfallibleTimesSync for T {
+    #[inline]
+    fn add_current(&mut self, timestamp: Self::Timestamp) -> bool {
+        // SAFETY: It is physically impossible for users to express `Err(...)` due to the inability
+        // to construct `Infallible`
+        unsafe { <T as TimesSync>::add_current(self, timestamp).unwrap_unchecked() }
+    }
+}
+
+
+
 
 
 /***** LIBRARY *****/
-/// Extends a [`Set`] of [`Timestamp`]s with functionality to see if an agreement is valid at this
-/// timestamp.
+/// Extends a [`Set`] of timestamps with functionality to see which ones are current.
 #[pointer_impls]
 pub trait Times: Set<Self::Timestamp> {
+    /// The type of the returned subset of current times.
+    type Subset: Set<Self::Timestamp>;
     /// The timestamp representation used in this set.
     type Timestamp: Eq + Hash + Ord;
 
 
-    /// Checks if a given timed object is valid at the current time.
+    /// Returns all the current times.
     ///
     /// This is used by auditors or other agents at the moment they first receive an enactment to
-    /// see if it was done using a current agreement.
-    ///
-    /// # Arguments
-    /// - `obj`: A [`Timed`] object who's time will be checked against the current times.
+    /// see if it was done using a current agreement. I.e., if the action/agreement was at a time
+    /// which is in the returned set, it is considered current.
     ///
     /// # Returns
-    /// True if it was valid, or false otherwise.
+    /// Some set of [`Self::Timestamp`](Times::Timestamp)s that indicate which are current.
     ///
     /// # Errors
     /// This function may error at any time the implementation likes.
-    fn is_valid(&self, obj: impl Timed) -> Result<bool, Self::Error>;
+    fn current(&self) -> Result<Self::Subset, Self::Error>;
+}
+
+
+
+/// Extends a [`SetSync`] of timestamps with functionality to define which ones are current.
+#[pointer_impls]
+pub trait TimesSync: SetSync<Self::Timestamp> + Times {
+    /// Adds a given timestamp to the current ones.
+    ///
+    /// This function should always imply also calling [`SetSync::add()`] for the given element.
+    /// Remember, after all, that the set of current times needs to be a _subset_ of all times.
+    ///
+    /// # Arguments
+    /// - `timestamp`: A new [`Times::Timestamp`] to mark as current.
+    ///
+    /// # Returns
+    /// True if the element was already marked as current, or false otherwise.
+    ///
+    /// # Errors
+    /// This function may error at any time the implementation likes.
+    fn add_current(&mut self, timestamp: Self::Timestamp) -> Result<bool, Self::Error>;
 }
