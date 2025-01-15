7cd0c1367e82d27827b7af2e77e04878811cd2db
Lut99 <mullertim99@gmail.com>
2025-01-15 17:42:06 +0100

    `Selector` is now serializable _and_ mappable.

--- src/collections/mod.rs
+++ src/collections/mod.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    13 Jan 2025, 16:22:05
 //  Last edited:
-//    15 Jan 2025, 17:34:03
+//    15 Jan 2025, 17:41:43
 //  Auto updated?
 //    Yes
 //
@@ -20,9 +20,27 @@ pub mod set;
 /***** LIBRARY *****/
 /// Defines ways for agents to choose who to send updates to.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub enum Selector<I> {
     /// Send it to a specific agent.
     Agent(I),
     /// Send it to all agents.
     All,
+}
+impl<I> Selector<I> {
+    /// Maps the identifier of the agent, if any.
+    ///
+    /// # Arguments
+    /// - `callback`: Some [`FnOnce`] that will translate `I` to something else. Note it is only
+    ///   called if this is a [`Selector::Agent`].
+    ///
+    /// # Returns
+    /// A new Selector with the mapped identifier of an agent.
+    #[inline]
+    pub fn map<T>(self, callback: impl FnOnce(I) -> T) -> Selector<T> {
+        match self {
+            Self::Agent(id) => Selector::Agent(callback(id)),
+            Self::All => Selector::All,
+        }
+    }
 }
